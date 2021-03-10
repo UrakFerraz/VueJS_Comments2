f30c6117de8463b44b58b569c799a50275e3d3be
Urak <33769257+UrakFerraz@users.noreply.github.com>
2021-03-10 00:11:42 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="hero is-primary">
+    <div class="hero-body">
+      <p class="title">
+        Comentários
+      </p>
+      <p class="subtitle">
+        Teste vue-cli VueJS
+      </p>
+    </div>
+  </section>
+  <CommentsInput :comments="this.comments"/>
+  <CommentsList :comments="this.comments"/>
+</template>
+
+<script>
+import './assets/bulma.min.css'
+import CommentsInput from './components/CommentsInput'
+import CommentsList from './components/CommentsList'
+
+export default {
+  name: 'App',
+  components: {
+    CommentsInput,
+    CommentsList
+  },
+  data() {
+    return {
+      comments: []
+    }
+  }
+}
+</script>
+
+<style>
+.block:not(:last-child) {
+  margin-bottom: 1.5rem;
+}
+</style>
---
+++ src/components/CommentsList.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="block" :key="comment.id" v-for="(comment, index) in comments">
+          <Comment :comment="comment" @delComment="delComment(index)"/>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import Comment from './Comment'
+export default {
+    props: {
+        comments: Array
+    },
+    methods: {
+        delComment(id) {
+        this.comments.splice(id, 1);
+        }
+    },
+    components: {
+        Comment
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
